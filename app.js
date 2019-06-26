aa8b6300f72637927c3699af208e7fbbdd391743
kraki135 <kraki135@gmail.com>
2019-06-26 07:27:14 +0200

    z domu kod podstawowy

---
+++ app.js
@@ -0,0 +1,9 @@
+var btn_edit = document.querySelector('.btn-edit');
+var btn_cancel = document.querySelector('.btn-cancel')
+var btn_confirm = document.querySelector('.btn-confirm');
+var modal = document.querySelector('.modal');
+var backdrop = document.querySelector('.backdrop');
+
+btn_edit.addEventListener('click', function() {
+    
+});
